8f391a9ed91f468ec63662a39dca4e5e716929d9
itookyourboo <Wignorbo@yandex.ru>
2020-06-13 21:22:44 +0300

    New states and extended tuples

--- state.py
+++ state.py
@@ -3,6 +3,12 @@ from enum import Enum
 
 class State(Enum):
     MENU = 0
+    CHOOSE_TOP = 1
+    CHOOSE_NEW = 2
+    CHOOSE_RND = 3
+    SEARCH = 4
 
 
 State.ALL = tuple(State)
+State.CHOOSE = State.CHOOSE_TOP, State.CHOOSE_RND, State.CHOOSE_NEW
+State.TEST_CALL = State.MENU, State.CHOOSE_TOP, State.CHOOSE_RND, State.CHOOSE_NEW
