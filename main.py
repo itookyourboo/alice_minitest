6c448e98c9a02c86d40287081d26ce6b71cfca03
itookyourboo <Wignorbo@yandex.ru>
2020-06-19 21:27:54 +0300

    Test searching and "repeat" command

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 from base_skill.skill import *
 from .strings import *
 from .state import State
-from .test_helper import get_new_test, get_top_test, get_random_test, COUNT
-from .ui_helper import default_buttons
+from .test_helper import get_new_test, get_top_test, get_random_test, add_wtf, try_to_find_test, morph_words
+from .ui_helper import default_buttons, save_last_text, normalize_tts
 
 
 handler = CommandHandler()
 
 
 @handler.hello_command
+@save_last_text
+@normalize_tts
 @default_buttons
 def hello(req, res, session):
     res.text = txt(TEXT_HELLO)
     session['state'] = State.MENU
 
 
+@handler.undefined_command(states=State.MENU)
+@save_last_text
+@default_buttons
+def find_test(req, res, session):
+    test = try_to_find_test(req.text)
+    if test:
+        res.text = txt(TEXT_FOUND_1).format(test.name, test.description)
+        session['test'] = test
+        session['state'] = State.CHOOSE_FOUND
+    else:
+        res.text = txt(TEXT_NOT_FOUND)
+
+
 @handler.command(words=tkn(WORDS_NEW), states=State.TEST_CALL)
+@save_last_text
 @default_buttons
 def test_new(req, res, session):
     show_new_test(req, res, session, intro=True)
@@ -23,6 +39,7 @@ def test_new(req, res, session):
 
 
 @handler.command(words=tkn(WORDS_TOP), states=State.TEST_CALL)
+@save_last_text
 @default_buttons
 def test_top(req, res, session):
     show_top_test(req, res, session, intro=True)
@@ -30,6 +47,7 @@ def test_top(req, res, session):
 
 
 @handler.command(words=tkn(WORDS_RND), states=State.TEST_CALL)
+@save_last_text
 @default_buttons
 def test_rnd(req, res, session):
     show_rnd_test(req, res, session, intro=True)
@@ -37,6 +55,7 @@ def test_rnd(req, res, session):
 
 
 @handler.command(words=tkn(WORDS_NEXT), states=State.CHOOSE)
+@save_last_text
 @default_buttons
 def next_test(req, res, session):
     args = req, res, session, False
@@ -44,10 +63,12 @@ def next_test(req, res, session):
         State.CHOOSE_NEW: show_new_test,
         State.CHOOSE_TOP: show_top_test,
         State.CHOOSE_RND: show_rnd_test,
+        State.CHOOSE_FOUND: exit_found_test
     }[session['state']](*args)
 
 
 @handler.command(words=tkn(WORDS_PASS), states=State.CHOOSE)
+@save_last_text
 @default_buttons
 def enter_the_name(req, res, session):
     res.text = txt(TEXT_ENTER_THE_NAME)
@@ -55,6 +76,7 @@ def enter_the_name(req, res, session):
 
 
 @handler.undefined_command(states=State.PASS_TEST)
+@save_last_text
 @default_buttons
 def check_name(req, res, session):
     contains_name = False
@@ -64,14 +86,14 @@ def check_name(req, res, session):
             contains_name = True
             break
 
-    if not contains_name:
+    if contains_name or 'name' in session and any(x in req.tokens for x in tkn(WORDS_PASS)):
+        start_test(req, res, session)
+    else:
         res.text = txt(TEXT_NO_NAME)
-        return
-
-    start_test(req, res, session)
 
 
 @handler.command(words=tkn(WORDS_LIKE), states=State.PASS_TEST)
+@save_last_text
 @default_buttons
 def like(req, res, session):
     test = session['test']
@@ -83,6 +105,7 @@ def like(req, res, session):
 
 
 @handler.command(words=tkn(WORDS_EXIT), states=State.CHOOSE + (State.PASS_TEST,))
+@save_last_text
 @default_buttons
 def back(req, res, session):
     session['state'] = State.MENU
@@ -90,12 +113,14 @@ def back(req, res, session):
 
 
 @handler.command(words=tkn(WORDS_HELP), states=State.ALL)
+@save_last_text
 @default_buttons
 def help_(req, res, session):
     res.text = txt(TEXT_HELP[session['state']])
 
 
 @handler.command(words=tkn(WORDS_ABILITY), states=State.ALL)
+@save_last_text
 @default_buttons
 def ability_(req, res, session):
     res.text = txt(TEXT_ABILITY)
@@ -108,10 +133,20 @@ def leave_skill(req, res, session):
     session.clear()
 
 
+@handler.command(words=tkn(WORDS_REPEAT), states=State.ALL)
+@save_last_text
+@default_buttons
+def repeat(req, res, session):
+    res.text = session['last_text']
+
+
 @handler.undefined_command(states=State.ALL)
+@save_last_text
+@normalize_tts
 @default_buttons
 def ne_ponel(req, res, session):
     res.text = txt(TEXT_NE_PONEL)
+    add_wtf(f'{session["state"]}: {req.text}')
 
 
 def show_new_test(req, res, session, intro=False):
@@ -129,6 +164,11 @@ def show_top_test(req, res, session, intro=False):
 def show_rnd_test(req, res, session, intro=False):
     test = get_random_test()
     show_test_base(res, session, test, intro=intro)
+
+
+def exit_found_test(req, res, session, intro=False):
+    session['state'] = State.MENU
+    res.text = txt(TEXT_BACK)
 
 
 def show_test_base(res, session, test, intro=False, cycled=False):
