6abef432417d30c3663c73ac4a74cf0bf4a6d7ca
itookyourboo <Wignorbo@yandex.ru>
2020-06-19 21:26:37 +0300

    Helpful decorators

--- ui_helper.py
+++ ui_helper.py
@@ -1,3 +1,5 @@
+import os
+
 from base_skill.skill import button
 from .strings import btn, BUTTONS
 
@@ -11,4 +13,19 @@ def default_buttons(func):
             for x in btn(BUTTONS[session['state']]):
                 res.buttons.append(button(x))
 
+    return wrapper
+
+
+def save_last_text(func):
+    def wrapper(req, res, session):
+        func(req, res, session)
+        session['last_text'] = res.text
+
+    return wrapper
+
+
+def normalize_tts(func):
+    def wrapper(req, res, session):
+        func(req, res, session)
+        res.tts = res.text.replace('тест', 'т+эст')
     return wrapper
