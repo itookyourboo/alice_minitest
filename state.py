b0d5e38c64a40e2f69db81762db23b7a538f7f98
itookyourboo <Wignorbo@yandex.ru>
2020-06-19 21:25:41 +0300

    CHOOSE_FOUND

--- state.py
+++ state.py
@@ -7,8 +7,9 @@ class State(Enum):
     CHOOSE_NEW = 2
     CHOOSE_RND = 3
     PASS_TEST = 4
+    CHOOSE_FOUND = 5
 
 
 State.ALL = tuple(State)
-State.CHOOSE = State.CHOOSE_TOP, State.CHOOSE_RND, State.CHOOSE_NEW
+State.CHOOSE = State.CHOOSE_TOP, State.CHOOSE_RND, State.CHOOSE_NEW, State.CHOOSE_FOUND
 State.TEST_CALL = State.MENU, State.CHOOSE_TOP, State.CHOOSE_RND, State.CHOOSE_NEW
