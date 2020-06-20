dbd17738f956154ce386fd75c9b5c91ded317eff
itookyourboo <Wignorbo@yandex.ru>
2020-06-20 16:39:45 +0300

    Cards and random images

--- ui_helper.py
+++ ui_helper.py
@@ -1,7 +1,30 @@
 import os
+from random import choice
 
-from base_skill.skill import button
+from base_skill.skill import button, BigImageCard
 from .strings import btn, BUTTONS
+
+
+uni_images = (
+    '1652229/d198ef1eb3711123dec9',
+    '213044/45c4186ffe6e4082104e',
+    '213044/d66b844d27d8fc3cd77d',
+    '1652229/4153ac331be307dde553',
+    '965417/932b2c855f611eca6a8b',
+    '1652229/47c83d85431693b69cda'
+)
+
+
+def get_random_image():
+    return choice(uni_images)
+
+
+def get_card(title, description, image_id=None):
+    card = BigImageCard()
+    card.title = title
+    card.description = description
+    card.image_id = image_id if image_id else get_random_image()
+    return card
 
 
 def default_buttons(func):
@@ -16,10 +39,17 @@ def default_buttons(func):
     return wrapper
 
 
-def save_last_text(func):
+def save_res(func):
     def wrapper(req, res, session):
         func(req, res, session)
+        session['last_card'] = None
+        session['last_text'] = None
+        session['last_tts'] = None
+
+        if res.card:
+            session['last_card'] = res.card.card
         session['last_text'] = res.text
+        session['last_tts'] = res.tts
 
     return wrapper
 
